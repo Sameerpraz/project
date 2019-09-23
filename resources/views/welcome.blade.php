622720c96ee636773b99b02d08227d24b73bbcba
Sameer Prajapti <samirpar788@gamil.com>
2019-09-23 15:46:52 +0545

    seed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Business Directory
                 </div>
 
                 <div class="links">
