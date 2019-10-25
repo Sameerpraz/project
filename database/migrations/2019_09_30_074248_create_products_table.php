7a89e2db6b633fb72a66fb9f7a4d5a9f2a1cbe33
Sameerpraz <samirpar788@gamil.com>
2019-10-25 10:30:44 +0545

    database

---
+++ database/migrations/2019_09_30_074248_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name')->unique();
+            $table->string('product_title');
+            $table->unsignedBigInteger('uid')->unsigned();
+            $table->foreign('uid')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('product_slug')->unique();
+            $table->string('product_posted_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
